0a82e2831dda4d5e6336e02658ad96301130e81e
truong <dntruongcoder@gmail.com>
2026-06-02 16:05:33 +0700

    update code adapt with env

--- web/backend/src/config/db.js
+++ web/backend/src/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    (await mongoose.connect(process.env.MONGODB_CONECTION_STRING, {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
       dbName: process.env.DB_DATABASE,
-    }),
-      console.log("Liên kết CSDL thành công !!!!"));
+    });
+    console.log("Liên kết CSDL thành công !!!!");
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(">>>>>>Error nè:", error);
